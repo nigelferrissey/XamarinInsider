b7dc78ecfc8a588e15d52f2cb4f0bc03947dc891
Nigel Ferrissey <nigel@ferrissey.com>
2017-07-12 20:23:52 +1000

    Font Icon Example

---
+++ FontIconApp/FontIconApp/FontIconApp.Android/CustomRenderers/FontAwesomeLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Graphics;
+using FontIconApp.Droid.CustomRenderers;
+using FontIconApp.UserControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(FontAwesomeLabel), typeof(FontAwesomeLabelRenderer))]
+
+namespace FontIconApp.Droid.CustomRenderers
+{
+    public class FontAwesomeLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Typeface = Typeface.CreateFromAsset(Forms.Context.Assets,
+                    "Fonts/" + FontAwesomeLabel.FontAwesomeName + ".otf");
+            }
+        }
+    }
+}
---
+++ FontIconApp/FontIconApp/FontIconApp/UserControls/FontAwesomeLabel.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace FontIconApp.UserControls
+{
+    public class FontAwesomeLabel : Label
+    {
+        public static readonly string FontAwesomeName = "FontAwesome";
+
+        //Parameterless constructor for XAML
+        public FontAwesomeLabel()
+        {
+            FontFamily = FontAwesomeName;
+        }
+
+        public FontAwesomeLabel(string fontAwesomeLabel = null)
+        {
+            FontFamily = FontAwesomeName;
+            Text = fontAwesomeLabel;
+        }
+    }
+
+    // https://github.com/neilkennedy/FontAwesome.Xamarin/blob/master/FontAwesome.Xamarin/FontAwesome.cs
+    // For a huge list of icon codes
+    public static class Icon
+    {
+        public static readonly string FAGlass = "\uf000";
+        public static readonly string FAMusic = "\uf001";
+        public static readonly string FASearch = "\uf002";
+        public static readonly string FAEnvelopeO = "\uf003";
+    }
+}
